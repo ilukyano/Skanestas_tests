df08abd95c5808aed941bbf2ab89ce1c47108ca0
i.Lukyanov <i.Lukyanov@x5.ru>
2022-11-17 09:48:35 +0300

    .

--- datagen/datagen.py
+++ datagen/datagen.py
@@ -57,7 +57,7 @@ producer = KafkaProducer(bootstrap_servers = kbl)
 
 while True:
     i=gendata().__str__()
-    ack = producer.send(topic=tn, value=bytes(i, encoding='utf-8'))
+    ack = producer.send(topic=tn, value=bytes(i, encoding='utf-8'), partition=3)
     metadata = ack.get()
     #print(metadata.topic)
     #print(metadata.partition)
