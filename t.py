4b5c2b8b36e462b5bd102f1162979ac95d91389d
i.Lukyanov <i.Lukyanov@x5.ru>
2022-11-20 05:30:44 +0300

    .

--- t.py
+++ t.py
@@ -1,23 +1,55 @@
-import json
+import clickhouse_connect
+import datetime
+import logging
 
-# a=[]
-# b={}
-# c={}
-# c["dcdcwdc"]="ddccd"
-# c["bgb"]="44"
-# c["sdasf"]="sbdbdlrnh;krmn"
+#client = clickhouse_connect.get_client(host='stage-dbs02.vprok.tech', port=8123, username='default', password='123qweasd')
+#result = client.query("select host_name, host_address from system.clusters where cluster='skanestas'")
+#result = client.query("select bid_01, ask_01 from  skanestas.skanestas_tests_mv")
+#host_name=result.result_set
+#if host_name.__str__() != '[]':
+#    print(host_name[1])
+ch_addr='stage-dbs02.vprok.tech'
+ch_port=8123
+ch_user='default'
+ch_pass='123qweasd'
+ch_cluster='skanestas'
+ch_db='skanestas'
 
+def main():
+    check_db()
 
-# b["ddddd"]="222"
-# b["ccccc"]="4444"
-# a.append(b)
-# c[""]=a
-# print(c)
+def clickhouse(host, port, user, passwd, query):
+    try:    
+        with clickhouse_connect.get_client(host=host, port=port, username=user, password=passwd) as connect:
+            res=connect.query(query)
+        return res.result_set
+    except Exception as e:
+        logging.error(e)
 
-u='{"timestamp": 1668685411.628779, "bid_01": 9.323905587706939, "bid_02": 7.9034600014414735, "bid_50": 461.5926223199698, "ask_01": 5.2170391548985995, "ask_02": 4.647089812306192, "ask_50": 126.86858327018217, "stats": [159.60666263637273, 45.577570745795654]}'
+def cluster(): #Собираем все ноды кластера
+    query='%s%s%s' %("select host_name, host_address from system.clusters where cluster='",ch_cluster,"'" )
+    res=clickhouse(ch_addr, ch_port, ch_user, ch_pass, query)
+    return res
 
-def chk_skanestas_cond(str):
-    d=json.dumps(str)
-    return a
+def check_db():
+    nodes=cluster()
+    query='%s%s%s' %("select name from system.databases where name='",ch_db,"'" )
+    print("sss")
+    for i in nodes:
+        ch_addr=i[0]
+        res=clickhouse(ch_addr, ch_port, ch_user, ch_pass, query) #Проверяем есть ли БД на ноде
+        if res.__str__() == "[]":
+            query='%s%s%s%s' %("CREATE DATABASE if not exists ",ch_db," on cluster ", ch_cluster )
+            clickhouse(ch_addr, ch_port, ch_user, ch_pass, query)#Создаем БД на ноде с он кластер
     
-print(chk_skanestas_cond(u))
+def check_table():
+    query='%s%s%s' %("select name from system.tables where database='",ch_db,"'" )
+    
+
+    
+
+
+
+main()    
+    
+ 
